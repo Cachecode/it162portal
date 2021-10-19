8630aef1b61cf0ad87f1f56db4c84bf480cb42b3
Matthieu <77665167+Cachecode@users.noreply.github.com>
2021-10-18 20:29:12 -0700

    main drop

---
+++ index/php/contact.php
@@ -0,0 +1,18 @@
+
+    
+    
+    <div class="content"
+    
+        <?php
+        
+        include 'includes/contact_include.php'; #site keys & code here
+        
+        $toAddress = "mattfelker206@outlook.com";  //place your/your client's email address here
+        $toName = "Matthieu Felker"; //place your client's name here
+        $website = "Contact Test site81";  //place NAME of your client's website
+        
+        //echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('includes/simple.php');#demonstrates multiple form elements
+        
+        ?>
+    </div>
---
+++ index/php/header.php
@@ -0,0 +1,1 @@
+<?php include 'portal-config.php'?>
