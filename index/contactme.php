2079b2f0ee8d0cd02d493577061f9ce545e9fdf6
Matthieu <77665167+Cachecode@users.noreply.github.com>
2021-10-21 14:41:23 -0700

    broke

--- index/contactme.php
+++ index/contactme.php
@@ -11,17 +11,6 @@
     <link rel="stylesheet" href="css/forms.css" />
 </head>
 
-<head>
-    <title>IT162 Portal - Edit this code to make it your own!</title>
-    <meta name="viewport" content="width=device-width" />
-    <meta name="robots" content="noindex,nofollow" />
-    <meta charset="utf-8">
-    <script src="https://use.fontawesome.com/6a71565c22.js"></script>
-    <link rel="stylesheet" href="css/nav.css" />
-    <link rel="stylesheet" href="css/portal.css" />
-    <link rel="stylesheet" href="css/form.css" />
-</head>
-
 <body>
     <!-- START WRAPPER -->
     <main class="wrapper">
@@ -42,22 +31,63 @@
             </nav>
         </header>
 
-        <div class="formbox">
+        <section class='formsection'>
+            <div class="formbox">
+                <h1>contact</h1>
 
-            <?php
+                <?php
 
-            include 'includes/contact_include.php'; #site keys & code here
+                include 'includes/contact_include.php'; #site keys & code here
 
-            $toAddress = "mattfelker206@outlook.com";  //place your/your client's email address here
-            $toName = "Matthieu Felker"; //place your client's name here
-            $website = "Matthieu Felker's Portal Website";  //place NAME of your client's 
-            echo loadContact('simple.php');
+                $toAddress = "mattfelker206@outlook.com";  //place your/your client's email address here
+                $toName = "Matthieu Felker"; //place your client's name here
+                $website = "Matthieu Felker's Portal Website";  //place NAME of your client's 
+                echo loadContact('simple.php');
 
-            ?>
-        </div>
+                ?>
+            </div>
+
+            <div class="github-card" data-github="Cachecode" data-width="100%" data-theme="default"></div>
+            <ul class="textbox quick-link nav-item">
+                <small>
+                    <h3>Links</h3>
+                </small>
+                <li class="nav-item">
+                    <a class="quick-link active" href="https://www.linkedin.com/in/matthieufelker">Linkdin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Github</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Stackoverflow</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Tumblr</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Instagram</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Twitter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Steam</a>
+                </li>
+                <li class="nav-item">
+                    <a class="quick-link" href="#">Reddit</a>
+                </li>
+                </li>
+            </ul>
         </section>
 
+        <aside class='formaside'>
+            <h2>Right Column</h2>
 
+            <a href="index.php"><img src="images/logo.png" class="contactlogo floating" alt="Home Logo"></i>
+
+
+                <p>Lorem Ipsum place holder text goes here for now</p>
+                <p>Lorem Ipsum place holder text goes here for now</p>
 
 
 
@@ -79,3 +109,5 @@
 
 
 </html>
+
+<script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
